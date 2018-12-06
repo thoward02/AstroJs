bb05b228a9ae712f0e15aadd168cb45cefedfb8d
thoward02 <36546676+thoward02@users.noreply.github.com>
2018-12-06 12:22:12 -0500

    Fixed the enemy loop smile

--- Source/Questions.js
+++ Source/Questions.js
@@ -1,21 +1,21 @@
 //This will control the basic questions <3
 
-questions = [
+window.questions = [
   "He was as fast as a lightning bolt",
   "He was a walking dictionary",
   "Boom",
   "Break a leg"
 ]
 
-anw = [
+window.anw = [
   "Simile",
-  "Metaphore",
+  "Metaphor",
   "Onomatopoeia",
   "Idiom"
 ]
 
-Cquestions = questions;
-Canw = anw;
+Cquestions = window.questions;
+Canw = window.anw;
 
 function SetUpQuestions(){
 
@@ -23,24 +23,40 @@ function SetUpQuestions(){
   QBox = document.getElementById("question");
 
   //Set up the question
-  CurrentQuestionId = Math.floor(Math.random() * questions.length);
+  CurrentQuestionId = Math.floor(Math.random() * window.questions.length);
   CurrentQuestion = Cquestions[CurrentQuestionId];
+
   QBox.innerHTML = 'What figure of speech is this: "' + CurrentQuestion + '" ?';
-  ListOfAnw = Canw;
-  ListOfAnw.splice(CurrentQuestionId, 1)
 
   //Set up Anwsers
   for(var num in window.renderObjects.Enemy){
 
     if(window.renderObjects.Enemy[num].right == 0){
-      window.renderObjects.Enemy[num].enemybox.innerHTML = anw[CurrentQuestionId];
-      console.log(CurrentQuestionId + anw)
+      window.renderObjects.Enemy[num].enemybox.innerHTML = Canw[CurrentQuestionId];
+
+      //Take out the Anwser
+      Canw.splice(CurrentQuestionId, CurrentQuestionId)
+
+      //Append
       document.body.appendChild(window.renderObjects.Enemy[num].enemybox);
+
     }else{
-      console.log("y")
-      anwId = Math.floor(Math.random() * ListOfAnw.length)
-      anwS = ListOfAnw[anwId];
-      ListOfAnw.splice(anwId, 1);
+      //If this block isnt the anw
+
+
+      NonAnwserId = CurrentQuestionId;
+
+      //This is to prevent the anwser from appearing
+      while(NonAnwserId == CurrentQuestionId){
+
+        NonAnwserId = Math.floor(Math.random() * Canw.length);
+
+      }
+
+      anwS = Canw[NonAnwserId];
+      //Remove fake anwser
+      Canw.splice(NonAnwserId, NonAnwserId);
+
 
       window.renderObjects.Enemy[num].enemybox.innerHTML = anwS;
       document.body.appendChild(window.renderObjects.Enemy[num].enemybox);
